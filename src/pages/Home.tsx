b0d1437f4632ac2d738c768510af4732c7ba9e73
mori-kei <mojavascript13@gmail.com>
2023-10-19 23:02:47 +0900

    LoginUserの型定義の変更と無駄な記述の削除

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,9 @@ import { useAuthContext } from "../context/authContext";
 import { Link } from "react-router-dom";
 const Home = () => {
   const { user ,logout} = useAuthContext();
-  console.log(user);
   return (
     <div>
-      <h2>Welcome home {user && user.id}</h2>
+      <h2>Welcome home {user && user.id }</h2>
       <div className="">
         <Link to={"/login"}>login</Link>
         <div onClick={logout}>ログアウト</div>
