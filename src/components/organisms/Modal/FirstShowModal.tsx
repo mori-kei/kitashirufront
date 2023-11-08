bc67b197c4b586bfdbd49737bb9afb389823c45f
mori-kei <mojavascript13@gmail.com>
2023-11-08 18:57:15 +0900

    default値がtrueのmodalコンポーネントの作成

---
+++ src/components/organisms/Modal/FirstShowModal.tsx
@@ -0,0 +1,63 @@
+import React, { ReactNode } from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+} from "@chakra-ui/react";
+import { useDefaultShow } from "../../../hooks/useDefaultShow";
+
+type Props = {
+  title: string;
+  openText: string;
+  closeText: string;
+  secondlyActionText?: string;
+  onClick?: () => void;
+  children: ReactNode;
+  size: string;
+  buttonWidth: string;
+};
+export const FirstShowModal = ({
+  title,
+  openText,
+  secondlyActionText,
+  closeText,
+  onClick,
+  children,
+  size,
+  buttonWidth,
+}: Props) => {
+  const { isOpen, onOpen, onClose } = useDefaultShow();
+  return (
+    <>
+      <Button width={buttonWidth} onClick={onOpen}>
+        {openText}
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose} size={size}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>{children}</ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              {closeText}
+            </Button>
+            {onClick ? (
+              <Button variant="ghost" onClick={onClick}>
+                {secondlyActionText}
+              </Button>
+            ) : null}
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
