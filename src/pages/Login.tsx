d7d3f1d891c7f1f3132d60f20af1d4f7d710391c
mori-kei <mojavascript13@gmail.com>
2023-12-01 16:20:44 +0900

    記述の変更

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -13,6 +13,7 @@ import {
   useColorModeValue,
 } from "@chakra-ui/react";
 import { useAuthContext } from "../context/authContext";
+import { HoverLink } from "../components/Atoms/Link/HoverLink";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -61,8 +62,9 @@ const Login = () => {
                 align={"start"}
                 justify={"space-between"}
               >
-                <Checkbox>Remember me</Checkbox>
-                <Text color={"blue.400"}>Forgot password?</Text>
+                <HoverLink to={"/signup"}>
+                  <Text color={"blue.400"}>新規登録はこちら</Text>
+                </HoverLink>
               </Stack>
               <Button
                 bg={"blue.400"}
@@ -72,7 +74,7 @@ const Login = () => {
                 }}
                 onClick={handleSubmit}
               >
-                LogIn
+                ログイン
               </Button>
             </Stack>
           </Stack>
